981be86e48fcccb50471bd0a90f08b9d3a1b0fe2
Diego Wright <dwright@viisi.cs.utexas.edu>
2017-05-02 21:14:18 -0500

    added tableview and delegate methods to plot view controller

--- Mobile Computing Project App/PlotViewController.swift
+++ Mobile Computing Project App/PlotViewController.swift
@@ -10,22 +10,23 @@
 import UIKit
 import JBChart
 
-class PlotViewController: UIViewController, JBBarChartViewDelegate, JBBarChartViewDataSource {
-    
-    @IBOutlet weak var informationLabel: UILabel!
- 
-    
+class PlotViewController: UIViewController, JBBarChartViewDelegate, JBBarChartViewDataSource, UITableViewDelegate, UITableViewDataSource {
+
     @IBOutlet weak var barChart: JBBarChartView!
     
+    @IBOutlet weak var tableView: UITableView!
+    var plots:[String] = []
     
     var chartLegend = ["11-14", "11-15", "11-16", "11-17", "11-18", "11-19", "11-20"]
     var chartData = [70, 80, 76, 88, 90, 69, 74]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         view.backgroundColor = UIColor.darkGray
+        
+        // TableView setup
+        tableView.delegate = self
+        tableView.dataSource = self
         
         // bar chart setup
         barChart.backgroundColor = UIColor.darkGray
@@ -35,22 +36,27 @@ class PlotViewController: UIViewController, JBBarChartViewDelegate, JBBarChartVi
         barChart.maximumValue = 100
         
         barChart.reloadData()
-        
         barChart.setState(.collapsed, animated: false)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        var footerView = UIView(frame: CGRect(x: 0, y: 0, width: barChart.frame.width, height: 16))
+        let footerView = UIView(frame: CGRect(x: 0, y: 0,
+                                              width: barChart.frame.width,
+                                              height: 16))
         
         print("viewDidLoad: \(barChart.frame.width)")
         
-        var footer1 = UILabel(frame: CGRect(x: 0, y: 0, width: barChart.frame.width/2 - 8, height: 16))
+        let footer1 = UILabel(frame: CGRect(x: 0, y: 0,
+                                            width: barChart.frame.width/2 - 8,
+                                            height: 16))
         footer1.textColor = UIColor.white
         footer1.text = "\(chartLegend[0])"
         
-        var footer2 = UILabel(frame: CGRect(x: barChart.frame.width/2 - 8, y: 0, width: barChart.frame.width/2 - 8, height: 16))
+        let footer2 = UILabel(frame: CGRect(x: barChart.frame.width/2 - 8, y: 0,
+                                            width: barChart.frame.width/2 - 8,
+                                            height: 16))
         footer2.textColor = UIColor.white
         footer2.text = "\(chartLegend[chartLegend.count - 1])"
         footer2.textAlignment = NSTextAlignment.right
@@ -58,7 +64,9 @@ class PlotViewController: UIViewController, JBBarChartViewDelegate, JBBarChartVi
         footerView.addSubview(footer1)
         footerView.addSubview(footer2)
         
-        var header = UILabel(frame: CGRect(x: 0, y: 0, width: barChart.frame.width, height: 50))
+        let header = UILabel(frame: CGRect(x: 0, y: 0,
+                                           width: barChart.frame.width,
+                                           height: 50))
         header.textColor = UIColor.white
         header.font = UIFont.systemFont(ofSize: 24)
         header.text = "Weather: San Jose, CA"
@@ -74,7 +82,10 @@ class PlotViewController: UIViewController, JBBarChartViewDelegate, JBBarChartVi
         // our code
         barChart.reloadData()
         
-        var timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(PlotViewController.showChart), userInfo: nil, repeats: false)
+        let timer = Timer.scheduledTimer(timeInterval: 0.5,
+                                         target: self,
+                                         selector:#selector(PlotViewController.showChart),
+                                         userInfo: nil, repeats: false)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -108,10 +119,27 @@ class PlotViewController: UIViewController, JBBarChartViewDelegate, JBBarChartVi
         let data = chartData[Int(index)]
         let key = chartLegend[Int(index)]
         
-        informationLabel.text = "Weather on \(key): \(data)"
+        //informationLabel.text = "Weather on \(key): \(data)"
+        return
     }
     
     func didDeselect(_ barChartView: JBBarChartView!) {
-        informationLabel.text = ""
+        //informationLabel.text = ""
+        return
+    }
+    
+    //Mark: - TableView
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.plots.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        return cell
     }
 }
